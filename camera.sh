e05123b0d47eaa42662db8bc4dedecb026336bbc
Roland <rsynnest@gmail.com>
2015-09-19 23:03:04 +0000

    updated camera to snap pictures once for each client/server request loop, instead of as a separate loop running independantly on the server

--- camera.sh
+++ camera.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-# Run this script with 
-# nohup ./camera.sh 0<&- &>/dev/null &
+# This script is called from server.js to snap a picture every time the client pings  socket (every 0.5 seconds)
 
-/usr/local/bin/node /var/www/camera/server.js &
-
-while true; do  
-  for i in `seq 1 30`
-  do  
-    # live stream a single file (overwrite every 3 seconds)
-    fswebcam -d /dev/video0 -r 320x240 live-webcam.jpg
-    sleep 0.1
-  done
-  # keep a backlog of images every 3 seconds
-  fswebcam -d /dev/video0 -r 320x240 webcam-%s.jpg
-done
+# live stream a single file to client (gets overwritten every time client pings socket)
+fswebcam -d /dev/video0 -r 320x240 live-webcam.jpg
+# keep a backlog of images every 3 seconds
+fswebcam -d /dev/video0 -r 320x240 ./archive/webcam-%s.jpg
 
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
+var util = require('util'),
+    exec = require('child_process').exec,
+    child;
+    
 
 var server = BinaryServer({port: 9000});
 console.log('server started');
@@ -7,6 +11,14 @@ server.on('connection', function(client){
   console.log('connection made');
   client.on('stream', function(s) {
     var stream = client.createStream();
+    
+    child = exec('./camera.sh',
+        function (error, stdout, stderr) {
+	    if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+	});
+
     var file = fs.createReadStream(__dirname + '/live-webcam.jpg');
     file.pipe(stream);
   });
